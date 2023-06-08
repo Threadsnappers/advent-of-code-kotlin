bdd795bd4be5eaef2f6cb8993c483be75333015f
The-Coder-Kishor <58686913+The-Coder-Kishor@users.noreply.github.com>
2023-06-08 18:36:14 +0530

    Added starlight template for docs

---
+++ astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import markdoc from '@astrojs/markdoc';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    markdoc(),
+    starlight({
+      title: 'Threadsnappers',
+      social: {
+        github: 'https://github.com/Threadsnappers/advent-of-code',
+      },
+      sidebar: [
+        {
+          label: 'Guides',
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: 'Example Guide', link: '/guides/example/' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
+});
