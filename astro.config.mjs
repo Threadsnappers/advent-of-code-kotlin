d8249ada2f7316c968c9044d05e86e5a07dd6b76
The-Coder-Kishor <58686913+The-Coder-Kishor@users.noreply.github.com>
2023-06-08 21:26:46 +0530

    feat: set up file structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,21 +7,46 @@ export default defineConfig({
   integrations: [
     markdoc(),
     starlight({
-      title: 'Threadsnappers',
+      title: 'Threadsnappers AoC',
+      editLink: {
+        baseUrl: 'https://github.com/Threadsnappers/advent-of-code-kotlin/edit/main/',
+      },
       social: {
-        github: 'https://github.com/Threadsnappers/advent-of-code',
+        github: 'https://github.com/Threadsnappers/advent-of-code-kotlin',
+        discord: 'https://discord.gg/NuKDmRUPRm'
       },
       sidebar: [
         {
-          label: 'Guides',
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: '/guides/example/' },
-          ],
+          label: 'AoC 2022',
+          autogenerate: { directory: 'aoc2022'}
         },
         {
-          label: 'Reference',
-          autogenerate: { directory: 'reference' },
+          label: 'AoC 2021',
+          autogenerate: { directory: 'aoc2021'}
+        },
+        {
+          label: 'AoC 2020',
+          autogenerate: { directory: 'aoc2020'}
+        },
+        {
+          label: 'AoC 2019',
+          autogenerate: { directory: 'aoc2019'}
+        },
+        {
+          label: 'AoC 2018',
+          autogenerate: { directory: 'aoc2018'}
+        },
+        {
+          label: 'AoC 2017',
+          autogenerate: { directory: 'aoc2017'}
+        },
+        {
+          label: 'AoC 2016',
+          autogenerate: { directory: 'aoc2016'}
+        },
+        {
+          label: 'AoC 2015',
+          autogenerate: { directory: 'aoc2015'}
         },
       ],
     }),
---
+++ src/content/docs/aoc2022/day1.md
@@ -0,0 +1,6 @@
+---
+title: Welcome to Threadsnappers AoC
+description: A Collection of all our attempts at solving Advent of Code problems.
+template: doc
+---
+Solutions to Advent of Code using Kotlin.
