be99b4e5d5f59e04781548d951839a67b94aedea
The-Coder-Kishor <58686913+The-Coder-Kishor@users.noreply.github.com>
2023-06-17 20:49:25 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -22,34 +22,6 @@ export default defineConfig({
           label: 'AoC 2022',
           autogenerate: { directory: 'aoc2022'}
         },
-        {
-          label: 'AoC 2021',
-          autogenerate: { directory: 'aoc2021'}
-        },
-        {
-          label: 'AoC 2020',
-          autogenerate: { directory: 'aoc2020'}
-        },
-        {
-          label: 'AoC 2019',
-          autogenerate: { directory: 'aoc2019'}
-        },
-        {
-          label: 'AoC 2018',
-          autogenerate: { directory: 'aoc2018'}
-        },
-        {
-          label: 'AoC 2017',
-          autogenerate: { directory: 'aoc2017'}
-        },
-        {
-          label: 'AoC 2016',
-          autogenerate: { directory: 'aoc2016'}
-        },
-        {
-          label: 'AoC 2015',
-          autogenerate: { directory: 'aoc2015'}
-        },
       ],
     }),
   ],
